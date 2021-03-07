70aa1be445083f7a965ad36cdc636ec89bf89b08
magiruuvelvet <magiruuvelvet@naver.com>
2021-03-07 13:01:02 +0100

    templates: update D template cmake minimum

--- templates/D_Project.cmake
+++ templates/D_Project.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.0)
+cmake_minimum_required(VERSION 3.14)
 set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
 
 # register D support

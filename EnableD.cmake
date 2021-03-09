ae5ab1ad4cc856edfce99c3c1e0c7b0a29e1944c
magiruuvelvet <magiruuvelvet@naver.com>
2021-03-09 18:55:30 +0100

    EnableD: custom version() macro

--- EnableD.cmake
+++ EnableD.cmake
@@ -1,2 +1,11 @@
 # register support for the D programming language in CMake
 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/LanguageSupport/D" "${CMAKE_MODULE_PATH}" CACHE PATH "" FORCE)
+
+# wrapper for defining custom version()s
+macro(d_define_version TARGET VERSION)
+    if ("${CMAKE_D_COMPILER_ID}" STREQUAL "LDC")
+        target_compile_options(TARGET PRIVATE "--d-version=${VERSION}")
+    else()
+        target_compile_options(TARGET PRIVATE "-version=${VERSION}")
+    endif()
+endmacro()

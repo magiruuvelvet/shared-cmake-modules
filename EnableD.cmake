20312fabbbad6f33e200c73a60caa9bf12fabd9b
magiruuvelvet <magiruuvelvet@naver.com>
2021-03-09 18:59:06 +0100

    EnableD: fix custom version() macro

--- EnableD.cmake
+++ EnableD.cmake
@@ -4,8 +4,8 @@ set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/LanguageSupport/D" "${CMAKE_MOD
 # wrapper for defining custom version()s
 macro(d_define_version TARGET VERSION)
     if ("${CMAKE_D_COMPILER_ID}" STREQUAL "LDC")
-        target_compile_options(TARGET PRIVATE "--d-version=${VERSION}")
+        target_compile_options(${TARGET} PRIVATE "--d-version=${VERSION}")
     else()
-        target_compile_options(TARGET PRIVATE "-version=${VERSION}")
+        target_compile_options(${TARGET} PRIVATE "-version=${VERSION}")
     endif()
 endmacro()

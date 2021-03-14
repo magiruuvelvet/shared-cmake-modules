dcbbf798d16f76d6a31df5e4793d395a7f8b2699
magiruuvelvet <magiruuvelvet@naver.com>
2021-03-14 09:13:16 +0100

    EnableD: don't hardcode scope in d_define_version

--- EnableD.cmake
+++ EnableD.cmake
@@ -2,10 +2,10 @@
 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/LanguageSupport/D" "${CMAKE_MODULE_PATH}" CACHE PATH "" FORCE)
 
 # wrapper for defining custom version()s
-macro(d_define_version TARGET VERSION)
+macro(d_define_version TARGET SCOPE VERSION)
     if ("${CMAKE_D_COMPILER_ID}" STREQUAL "LDC")
-        target_compile_options(${TARGET} PRIVATE "--d-version=${VERSION}")
+        target_compile_options(${TARGET} ${SCOPE} "--d-version=${VERSION}")
     else()
-        target_compile_options(${TARGET} PRIVATE "-version=${VERSION}")
+        target_compile_options(${TARGET} ${SCOPE} "-version=${VERSION}")
     endif()
 endmacro()
